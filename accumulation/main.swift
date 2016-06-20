d446998e3d094b2d1aded7b58f333ce297c643d0
20141105053w <20141105053w@admindeiMac-88.local>
2016-06-20 11:01:57 +0800

    accumulation
    从1累加到100，结果为5050.我最开始在网站中寻找swift中如何让应用循环。找到后开始学习，并且编程序。最开始输出为4950，发现是没有加
    100，便知道在循环中出现错误－－只循环到99，将”i<100”改为”i<=100”，输出结果正确

---
+++ accumulation/main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  accumulation
+//
+//  Created by 20141105053w on 16/6/20.
+//  Copyright © 2016年 20141105053w. All rights reserved.
+//
+
+
+import Foundation
+
+var num=0
+for(var i=0;i<=100;i+=1){
+    num+=i
+}
+print(num)
